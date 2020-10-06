4ff6f5f7a63fe102687fd896e28f61c891827a74
Arsenii Istlentev <ais@figaf.com>
2020-10-06 17:58:18 +0300

    IRT-1833 Support Cloud Foundry

--- src/main/java/com/figaf/plugin/tasks/DeployIntegrationFlow.java
+++ src/main/java/com/figaf/plugin/tasks/DeployIntegrationFlow.java
@@ -22,14 +22,14 @@ public class DeployIntegrationFlow extends AbstractIntegrationFlowTask {
     public void doTaskAction() throws Exception {
         System.out.println("deployIntegrationFlow");
         defineParameters(true);
-        String taskId = cpiClient.deployIFlow(cpiConnectionProperties, packageExternalId, integrationFlowExternalId, integrationFlowTechnicalName);
+        String taskId = integrationFlowClient.deployIFlow(cpiConnectionProperties, packageExternalId, integrationFlowExternalId, integrationFlowTechnicalName);
         if (waitForStartup == null || !waitForStartup) {
             return;
         }
         int numberOfAttempts = 1;
         boolean deployStatusSuccess = false;
         while (!deployStatusSuccess && numberOfAttempts <= MAX_NUMBER_OF_ATTEMPTS) {
-            String deployStatus = cpiClient.checkDeployStatus(cpiConnectionProperties, taskId);
+            String deployStatus = integrationFlowClient.checkDeployStatus(cpiConnectionProperties, taskId);
             System.out.println("deployStatus = " + deployStatus);
             if ("SUCCESS".equals(deployStatus)) {
                 deployStatusSuccess = true;
@@ -42,13 +42,13 @@ public class DeployIntegrationFlow extends AbstractIntegrationFlowTask {
         }
         boolean started = false;
         while (!started && numberOfAttempts <= MAX_NUMBER_OF_ATTEMPTS) {
-            IntegrationContent integrationContent = cpiClient.getIntegrationRuntimeArtifactByName(cpiConnectionProperties, integrationFlowTechnicalName);
+            IntegrationContent integrationContent = integrationRuntimeClient.getIntegrationRuntimeArtifactByName(cpiConnectionProperties, integrationFlowTechnicalName);
             String integrationRuntimeArtifactStatus = integrationContent.getStatus();
             System.out.println("integrationRuntimeArtifactStatus = " + integrationRuntimeArtifactStatus);
             if ("ERROR".equals(integrationRuntimeArtifactStatus)) {
                 List<String> errorMessages = null;
                 try {
-                    errorMessages = cpiClient.getIntegrationRuntimeErrorInformation(cpiConnectionProperties, integrationFlowTechnicalName);
+                    errorMessages = integrationRuntimeClient.getIntegrationRuntimeErrorInformation(cpiConnectionProperties, integrationContent);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
